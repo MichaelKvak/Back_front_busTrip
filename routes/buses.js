017ebc3a33195b693c8421ed100d56630a07fbf2
MichaelKvak <mkvak1987@gmail.com>
2021-09-05 02:30:23 +0300

    add poin commit

---
+++ routes/buses.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/bus_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+const busesScheme = new Schema({
+  destination_station: String,
+  depart_station: String,
+  duration_trip: Number,
+  ticket_price: Number,
+});
+
+const Bus = mongoose.model("Bus", busesScheme);
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  Bus.find({}, function (err, docs) {
+    if (err) return res.status(500).json({ err: { msg: "Fetch failed!" } });
+    res.render("bus_list", {
+      title: "Bus List",
+      list: docs,
+    });
+  });
+});
+router.get("/add", function (req, res, next) {
+  res.render("bus_add_form", {
+    title: "add bus",
+  });
+});
+router.post("/add", function (req, res, next) {
+  const bus = new Bus({
+    destination: req.body.busDestination,
+    depart: req.body.busDepart,
+    duration: parseInt(req.body.busDuration),
+    price: parseInt(req.body.busPrice),
+  });
+  bus.save(function (err, busDoc) {
+    if (err) return res.status(500).json({ err: { msg: "Saving failed!" } });
+    res.status(200).json({ success: true, busId: busDoc._id });
+  });
+});
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/bus_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const Schema = mongoose.Schema;
-const busesScheme = new Schema({
-  destination_station: String,
-  depart_station: String,
-  duration_trip: Number,
-  ticket_price: Number,
-});
-
-const Bus = mongoose.model("Bus", busesScheme);
-
-/* GET home page. */
+/* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-router.post("/", function (req, res, next) {
-const bus = new Bus({
-  destination_station: 
-  depart_station: 
-  duration_trip:
-  ticket_price:
-});
-
+  res.render("index", { title: "Home" });
 });
 
 module.exports = router;
